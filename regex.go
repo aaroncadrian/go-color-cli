79ec02b2fecd2e820fc522a7bfed4dd61e1a3ae7
Aaron Adrian <aaroncadrian@gmail.com>
2020-09-19 12:27:05 -0500

    Parse hex code

--- regex.go
+++ regex.go
@@ -6,11 +6,15 @@ import (
 )
 
 func main() {
-	hexCode := "00g99f"
+	hexCode := "#00f99f"
 
-	if utils.IsValidHexCode(hexCode) {
-		fmt.Println("This is a valid hex code!")
+	if code, isValid := utils.ParseHexCode(hexCode); isValid {
+		fmt.Printf("This is a valid hex code: %v\n", code)
 	} else {
 		fmt.Println("This is not a valid hex code.")
 	}
+
+	//r := regexp.MustCompile(`(?P<Year>\d{4})-(\d{2})-(?P<Day>\d{2})`)
+	//fmt.Printf("%#v\n", r.FindStringSubmatch(`2015-05-15`))
+	//fmt.Printf("%#v\n", r.SubexpNames())
 }
--- utils/valid_hex_code.go
+++ utils/valid_hex_code.go
@@ -1,9 +1,21 @@
 package utils
 
-import "regexp"
+import (
+	"regexp"
+)
 
-var hexCodeMatcher = regexp.MustCompile(`^#?[A-Fa-f0-9]{6}$`)
+var hexCodeMatcher = regexp.MustCompile(`^(?P<Hash>#?)(?P<HexCode>[A-Fa-f0-9]{6})$`)
 
 func IsValidHexCode(hexCode string) bool {
 	return hexCodeMatcher.MatchString(hexCode)
+}
+
+func ParseHexCode(hexCode string) (string, bool) {
+	if !IsValidHexCode(hexCode) {
+		return "", false
+	}
+
+	result := hexCodeMatcher.FindStringSubmatch(hexCode)
+
+	return result[2], true
 }
