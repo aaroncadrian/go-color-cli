ac4bf459f40fbfd3916538083cafe59351ac89f0
Aaron Adrian <aaroncadrian@gmail.com>
2020-09-19 12:00:59 -0500

    Require 2 arguments

--- cmd/add.go
+++ cmd/add.go
@@ -23,7 +23,7 @@ import (
 
 // addCmd represents the add command
 var addCmd = &cobra.Command{
-	Use:   "add",
+	Use:   "add [NAME] [HEXCODE]",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -31,7 +31,13 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
+	Args: cobra.ExactArgs(2),
 	Run: func(cmd *cobra.Command, args []string) {
+		name := args[0]
+		hexCode := args[1]
+
+		fmt.Printf("Adding %v as %v\n", name, hexCode)
+
 		fmt.Println("add called")
 	},
 }
